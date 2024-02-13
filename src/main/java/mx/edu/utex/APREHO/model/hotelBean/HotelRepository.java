c44f524c55924de3a0ebee27972a6f5f7fa4e420
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-13 12:22:12 -0600

    aver

---
+++ src/main/java/mx/edu/utex/APREHO/model/hotelBean/HotelRepository.java
@@ -0,0 +1,6 @@
+package mx.edu.utex.APREHO.model.hotelBean;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Long,Hotel> {
+}
