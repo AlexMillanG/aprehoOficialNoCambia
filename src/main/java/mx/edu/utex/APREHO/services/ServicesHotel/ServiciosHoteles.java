71d6a97c3e9df1c916b005786274b7ce6ba3ed8b
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-14 23:25:04 -0600

    lo itente pero me ganó el sueño

--- src/main/java/mx/edu/utex/APREHO/services/ServicesHotel/ServiciosHoteles.java
+++ src/main/java/mx/edu/utex/APREHO/services/ServicesHotel/ServiciosHoteles.java
@@ -1,4 +1,20 @@
 package mx.edu.utex.APREHO.services.ServicesHotel;
 
+import lombok.AllArgsConstructor;
+import mx.edu.utex.APREHO.config.ApiResponse;
+import mx.edu.utex.APREHO.model.hotelBean.Hotel;
+import mx.edu.utex.APREHO.model.hotelBean.HotelRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Transactional
 public class ServiciosHoteles {
+    private final HotelRepository hotelRepository;
+
+    public ResponseEntity<ApiResponse> saveHotel(Hotel hotel){
+        
+    }
 }
