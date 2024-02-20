685ccbc2c663762851f9fdbe820eb55e6fcfbcb6
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-20 12:22:59 -0600

    Perdón

---
+++ src/main/java/mx/edu/utex/APREHO/model/roomBean/RoomRepository.java
@@ -0,0 +1,6 @@
+package mx.edu.utex.APREHO.model.roomBean;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
--- src/main/java/mx/edu/utex/APREHO/services/ServicesRoom/ServiciosRoom.java
+++ src/main/java/mx/edu/utex/APREHO/services/ServicesRoom/ServiciosRoom.java
@@ -1,4 +1,16 @@
 package mx.edu.utex.APREHO.services.ServicesRoom;
 
+import lombok.AllArgsConstructor;
+import mx.edu.utex.APREHO.model.hotelBean.HotelRepository;
+import mx.edu.utex.APREHO.model.roomBean.RoomRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Transactional
+@Service
 public class ServiciosRoom {
+    private final RoomRepository roomRepository;
+    private final HotelRepository repository;
+    
 }
