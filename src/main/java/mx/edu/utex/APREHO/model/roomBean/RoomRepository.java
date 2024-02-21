32597623a17ea3b28f5ba14fcb3ab99871f2504b
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-20 23:03:17 -0600

    Volví a hacer los servicios puta madre
    Me gusta probar tu boca
    Me vuelve loco cuando dice
    Que soy el que te provoca
    Y ahora quiero verte desnuda
    Y si te dejas llevar, de mí
    Pasarás una noche de locura
    Vamos a hacerlo a tu tiempo
    Que yo quiero verte desnuda
    Y si te dejas llevar, de mí
    Pasarás una noche de locura
    Vamos a hacerlo a tu tiempo
    Mami si tu me calientas
    Y me prendes como un horno
    Puedo convertirme fácil
    En tu estrellita porno
    Verte la espalda sin ropa
    Paras la cola y me trastorno
    Tu novio te lo envío con
    Dos cuernos de adorno
    Y sin hablarte te voy a robar
    Para llevarte a un lugar
    Donde pueda poner a prueba
    Mi habilidad sexual
    Te voy atrapar, voy agarrarte
    Y no te voy a soltar quitarte
    El traje de baño que de aquí
    Tu no te va a escapar
    Tu baby problemática y el que
    Te estruja to'a la blusa
    Loco por ponértelo entremedio
    Pa' hacerte una rusa, pero pero
    No hay excusas siempre
    Los cuerpos se cruzan
    Te amarro y voy a ser tu cárcel
    Y tu mi reclusa
    Y ahora quiero verte desnuda
    Y si te dejas llevar de mí
    Pasarás una noche de locura
    Vamos a hacerlo a tu tiempo
    Que yo quiero verte desnuda
    Y si te dejas llevar, de mí
    Pasarás una noche de locura
    Vamos a hacerlo a tu tiempo
    Solo déjate llevar de mí
    Tienes el piquete
    De ese que me gusta a mí
    Flow de nena buena
    Siendo senda mala mami
    Tranquila tu vacila
    En mi cama
    Llevo tiempo detrás de ti
    Tienes el piquete de ese
    Que me gusta a mí
    Si tu marido no te quiere
    No te comprende el nene
    Te atiende ponte pa' mí
    Dile a tu gati gati gatito
    Que soy yo quien te coge
    Y te parte por lo mas finito
    Me traes bien loquito y yo
    Que no me quito te capturo
    Y en love machine te pongo a
    Dar gritos
    Dicen que lo bueno tarda
    Y quiero ver que hasta tu gordo esta
    En Colombia las nena ya
    Tienen las nalgas salda
    Hay alguno que te traspasa
    Como yo ninguno dile a tu
    Jevo que tengo balas
    Pa' mas de uno
    Ozuna y juan-k en el bloque
    Pa' que vean como esto vibra
    No soy heavy weight pero mi flow
    Pesa 500 libras
    Super yei pon la presión
    Que yo me siento un en sillón
    De esos costosos valorado
    En la fortuna de Donald Trump
    Cositas que te provocan
    Tengamos una aventura
    Quiero perderme en tu boca
    Bajemos la calentura
    Cositas que te provocan
    Tengamos una aventura
    Quiero perderme en tu boca
    Bajemos la calentura
    Y ahora quiero verte desnuda
    Y si te dejas llevar, de mí
    Pasarás una noche de locura
    Vamos a hacerlo a tu tiempo
    Super yei
    Nosotros somos superiority
    Ozuna O Z U N A el negrito de los ojos claros
    Y Juan-k el problemático
    El de la voz mas gruesa de la joyería
    Hahahaha
    Bless
    Siempre superior a ustedes
    Siempre por encima de ustedes
    Yomo pauta
    Wallner
    Jemmel el del dembow pesao
    Weight music
    Dimelo Ofal
    Musicologo y menes
    Los de la nazza
    Super yei bless

--- src/main/java/mx/edu/utex/APREHO/model/roomBean/RoomRepository.java
+++ src/main/java/mx/edu/utex/APREHO/model/roomBean/RoomRepository.java
@@ -2,5 +2,9 @@ package mx.edu.utex.APREHO.model.roomBean;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    Optional<Room> findByHotel_HotelId(Long l);
 }
