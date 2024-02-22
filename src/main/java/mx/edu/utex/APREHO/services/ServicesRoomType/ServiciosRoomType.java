4be1e07ac0f623d6b2ca781ff633174dd3637be6
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-22 13:21:50 -0600

    Pa'l party bajó con un conjunto rosa, pink Bailando se ve fabulosa, mami Yo soy caldoso y tú eres bien caldosa Toma gata celosa Hello Kitty Eso que traes ahí, mami, se te ve bien riquís Yo soy de arre, dale ma', vámonos riquís Si tú quieres wiki, te doy wiki-wiki A la gata le dije "Hello Kitty Eso que traes ahí, mami, se te ve bien riquís Yo soy de arre, dale ma', vámonos riquís Si tú quieres wiki, te doy wiki-wiki" Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- (M-A-L-I, el Malilla) Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Mano arriba ya llegó tu chacal, el que mueve el guacal Perreo pa' los nenes, perreo pa' las gyal Se ve que eres fina te dicen Bad Gyal (¿Y qué me vas a hacer papi?) Quiere que la azote y ese culote Pónmelo en la cara hasta que explote Se ve que eres mala, yo tu diablote Ponte perra diabla y yo malote Quiere que la azote, que la escupa, que la ahorque Que le agarre nalgas y que la choque Pónteme de espaldas y qué reboten Se ve que eres mala y yo diablote Quiere que la azote, que la escupa, que la ahorque Que le agarre nalgas y que la choque Pónteme de espaldas y qué reboten Se ve que eres mala y yo diablote Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón, reggaetón) Él te trajo Whiskey Yo te doy Don Periñon, bajé la Moet Chandon La Barbie no quiere Ken, quiere a su chacalón Un blunt y puti reggaetón Mi Hello Kitty por la city Fuma kripi, kripi kush Hello Kitty por la city Anda freaky, freakytona Hello Kitty, su color favo' es el pink Todas de rosa, anda con todo su team En la disco brillando como bling bling Como bling bling, como bling bling Hello Kitty, su color favo' es el pink Todas de rosa, anda con todo su team En la disco brillando como bling bling Como bling bling, como bling bling Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón) Vámono' ri-, vámono' ri-, vámono' ri-, vámono' ri- Vámono' ri-, vámono' ri-, vámono' riquís (reggaetón) Pa'l party bajó con un conjunto rosa, pink Bailando se ve fabulosa, mami Yo soy caldoso y tú eres bien caldosa Toma gata celosa Hello Kitty Eso que traes ahí mami, se te ve bien riquís Yo soy de arre, dale ma', vámonos riquís Si tú quieres wiki, te doy wiki-wiki Y a la gata le dije "Hello Kitty Eso que traes ahí, mami, se te ve bien riquís Yo soy de arre, dale ma', vámonos riquís Si tú quieres wiki, te doy wiki-wiki" Wiki y reggaetón Oye, el Bo M-A-L-I, el Malilla

--- src/main/java/mx/edu/utex/APREHO/services/ServicesRoomType/ServiciosRoomType.java
+++ src/main/java/mx/edu/utex/APREHO/services/ServicesRoomType/ServiciosRoomType.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import mx.edu.utex.APREHO.config.ApiResponse;
 import mx.edu.utex.APREHO.model.roomTypeBean.RoomType;
 import mx.edu.utex.APREHO.model.roomTypeBean.RoomTypeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +18,24 @@ public class ServiciosRoomType {
     private RoomTypeRepository roomTypeRepository;
 
     public ResponseEntity<ApiResponse> saveRoomType(RoomType roomType){
-        return null;
+        if (roomType.getPrice() <= 0)
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST,true ,"Error, precio no valido"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ApiResponse(roomTypeRepository.saveAndFlush(roomType),HttpStatus.BAD_REQUEST,true ,"Error, precio no valido"), HttpStatus.BAD_REQUEST);
     }
+
+    public ResponseEntity<ApiResponse> updateRoomType(RoomType roomType){
+        if (roomType.getPrice() <= 0)
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST,true ,"Error, precio no valido"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ApiResponse(roomTypeRepository.saveAndFlush(roomType),HttpStatus.BAD_REQUEST,true ,"Error, precio no valido"), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<ApiResponse> deleteRoomType(Long id){
+        Optional<RoomType> foundRoomType = roomTypeRepository.findById(id);
+        if (!foundRoomType.isPresent())
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST,true ,"Error, no se encontró el tipo de cuarto a eliminar"), HttpStatus.BAD_REQUEST);
+            roomTypeRepository.deleteById(id);
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,false ,"Tipo de cuarto eliminado con exito"), HttpStatus.BAD_REQUEST);
+    }
+
+    //select type_name from room inner join room_type on room_type.room_type_id =room.room_type_id inner join hotel on hotel.hotel_id =room.hotel_id;
 }
