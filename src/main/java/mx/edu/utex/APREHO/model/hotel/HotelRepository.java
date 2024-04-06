7dbfba184ada5d8c15eaf85604b15cd97e9c6cc7
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-04-05 21:07:05 -0600

    al parecer no era la etiqueta repository

--- src/main/java/mx/edu/utex/APREHO/model/hotel/HotelRepository.java
+++ src/main/java/mx/edu/utex/APREHO/model/hotel/HotelRepository.java
@@ -2,17 +2,18 @@ package mx.edu.utex.APREHO.model.hotel;
 
 import mx.edu.utex.APREHO.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel,Long> {
     Optional<Hotel> findByEmail(String email);
     List<Hotel> findByCity(String city);
     Optional<Hotel> deleteByEmail(String email);
 
     List<Hotel> findByUser(User user);
-    //List<Hotel> findAllByCity();
+   List<Hotel> findAllByCity();
 
 
 }
